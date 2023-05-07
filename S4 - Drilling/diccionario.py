f9b64cd6f9722663faac569569278baa584ef7c6
pupubeat <ratenas95@gmail.com>
2023-05-07 18:25:05 -0400

    Modulo 3 S4 Rebound y Drilling

---
+++ S4 - Drilling/diccionario.py
@@ -0,0 +1,32 @@
+# Atribuir valores a los 3 diccionarios
+
+Diccionario1 = {
+    "Nombre": "Yaneisi",
+    "Apellido": "Moscoso",
+    "Telefono":  56912345678
+}
+print(Diccionario1)
+
+Diccionario2 = {
+    "Nombre": "Pablo",
+    "Apellido": "Tiengdan",
+    "Telefono": 56956781234
+}
+print(Diccionario2)
+
+Diccionario3 = {
+    "Nombre": "Paloma",
+    "Apellido": "Atenas",
+    "Telefono": 56943218765
+}
+
+#Crear Lista vacia
+Lista = []
+
+#Colocar diccionarios en la lisla vacia
+Lista.append(Diccionario1)
+Lista.append(Diccionario2)
+Lista.append(Diccionario3)
+
+print(Lista)
+
---
+++ S4 - Rebound/ejercicio.py
@@ -0,0 +1,16 @@
+#Declarar variables
+a = 8
+b = 10.5
+c = "ejercicio"
+
+#Conjunto de variables a, b y c
+conjunto = {a,b,c}
+print(conjunto)
+
+#Conversión del conjunto a una lista
+lista = list(conjunto)
+print(lista)
+
+#Agregar variable False a la lista
+lista.append(False)
+print(lista)
