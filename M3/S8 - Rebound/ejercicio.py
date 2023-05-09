87d934d1439b7225a0ceb396d27808382bfae31c
pupubeat <ratenas95@gmail.com>
2023-05-08 23:40:46 -0400

    Ejercicio S8 M3

---
+++ M3/S8 - Rebound/ejercicio.py
@@ -0,0 +1,9 @@
+mi_lista = [5, 20, 15, 20, 25, 50, 20, 5, 18, 15]
+
+#Eliminar elementos repetidos
+mi_lista2= list(set(mi_lista))
+print(mi_lista2)
+
+#Ordenar elementos menor a mayor
+mi_lista2.sort()
+print(mi_lista2)
