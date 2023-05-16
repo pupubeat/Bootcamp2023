426dd3cf409c74fa71227e6947c428d217ca940a
pupubeat <ratenas95@gmail.com>
2023-05-15 23:59:53 -0400

    Agregar Drilling S5 M3

---
+++ M3/S5 - Drilling/ejercicio.py
@@ -0,0 +1,11 @@
+for x in "paralelepido":
+    if x == "a":
+        continue
+    elif x == "e":
+        continue
+    elif x == "i":
+        continue
+    elif x == "o":
+        continue
+    print(x, end="")
+print("")
