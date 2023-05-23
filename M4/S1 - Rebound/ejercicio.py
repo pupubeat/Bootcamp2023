cb87b510eae90afe75b493f73cebe794a9501ebc
pupubeat <ratenas95@gmail.com>
2023-05-23 00:24:17 -0400

    M4 S1 Rebound

---
+++ M4/S1 - Rebound/ejercicio.py
@@ -0,0 +1,28 @@
+class Animal():
+    Nombre = ""
+    Raza = ""
+    Edad = ""
+    Peso = ""
+    
+    def comer():
+        pass
+    def caminar():
+        pass
+    def dormir():
+        pass
+
+Perro = Animal()
+
+class Perro():
+    Nombre = "Brando"
+    Raza = "3 años"
+    Edad = "San Bernardo"
+    Peso = "30 Kg"
+
+Gato = Animal()
+
+class Gato():
+    Nombre = "Roll"
+    Raza = "4 años"
+    Edad = "Persa"
+    Peso = "3 Kg"
